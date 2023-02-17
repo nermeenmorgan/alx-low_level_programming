fb21200911305dbe0f300a3dd37360dfd2fc4e7f
Nemeen Fares <nermen.morgan99@gmail.com>
2023-02-16 18:57:22 -0800

    gg

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints prints the size of various types on the computer it is compiled and run on..
+ * Return: Always 0.
+ */
+
+int main(void) 
+{
+   	printf("size of a char: %zu byte(s)\n, sizeof(char));
+
+	printf("Size of an int: %zu  byte (s)\n", sizeof(int));
+	printf("Size of a long (s)\n", sizeof(int));
+}
