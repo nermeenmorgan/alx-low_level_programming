f53ea8567b3c1a7d51554c282c4c6b3a7ec0e1f2
Nemeen Fares <nermen.morgan99@gmail.com>
2023-02-22 17:01:36 -0800

    c task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,10 @@ int main(void)
 	int i = 0;
 
 	while (i < 8)
-       	{
-	      	_putchar(c[i]);
-	       	i++;
-       	}
-       	_putchar('\n');
-       	return (0);
+	{	
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
