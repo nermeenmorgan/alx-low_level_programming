f344e019bce53f423b4a7ceb90ffcd28d25b9d75
Nemeen Fares <nermen.morgan99@gmail.com>
2023-02-24 14:38:33 -0800

    c tasks

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,8 +17,10 @@ void print_diagonal(int n)
 		{
 			for (j = 0; j < n; j++)
 			{
-				if (j == i)  _putchar('\\');
-				else if (j < i)  _putchar(' ');
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 			}
 			_putchar('\n');
 		}
